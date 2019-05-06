d78725253e893f9b99d7fce78d1110eaddc31732
Marco Guilmette <marco.guilmette@softbiomed.com>
2019-05-06 15:23:54 -0400

    WIP

--- Part01-Fundamentals/app.js
+++ Part01-Fundamentals/app.js
@@ -6,7 +6,40 @@ function part13() {
 }
 
 function part14(){
+    let a = "5";
+    let b = "10"
 
+    console.log(Number(a) + Number(b));
 }
 
 
+function part17(){
+    let input = prompt("Entrer a value in miles");
+
+    if( isNaN(input) === false ) {
+        let number = Number(input);
+        alert(`The value is ${  number * 1.60945 } km`)
+    } else {
+        alert("Wrong input")
+    }
+}
+
+
+function part20() {
+    let number = 22;
+    console.log( number % 2 === 0 ? 'Even' : 'Odd');
+}
+
+
+function part24(){
+
+    let value = 2;
+
+    switch(value){
+        case 1:
+        case 3-2:
+
+
+    }
+
+}
