378a2cf833df9f3d99be545c30735f5dacea3fdd
Marco Guilmette <marco.guilmette@softbiomed.com>
2019-05-14 10:53:03 -0400

    Ultimate Dice Game + Simple Roll

--- Part10-Ultimate Dice/script.js
+++ Part10-Ultimate Dice/script.js
@@ -9,26 +9,44 @@ let computerScore = 0;
 let playerScore = 0;
 
 
-playerDice.innerText = "\u2685";
-computerDice.innerText = "\u2685";
+playerDice.innerText = "🎲";
+computerDice.innerText = "🎲";
 
 
 rollButton.addEventListener("click", (event) => {
 
-        let playerRoll = randomDice();
-        let computerRoll = randomDice();
+        rollButton.disabled = true;
+        let rebound = 15;
+        let timerId = window.setInterval( roll, 100 );
 
-        (playerRoll > computerRoll) ? playerScore++ : computerScore++;
+        function roll(){
 
-        playerTitle.innerText = `Player score : ${playerScore}`;
-        computerTitle.innerText = `Computer score : ${computerScore}`;
+            let computerRoll = randomDice();
+            let playerRoll = randomDice();
 
-        let playerUnicode = 9855 + playerRoll;
-        let computerUnicode = 9855 + computerRoll;
-        playerDice.innerHTML = "&#" + playerUnicode + ";";
-        computerDice.innerHTML = "&#" + computerUnicode + ";";
+            let playerUnicode = 9855 + playerRoll;
+            let computerUnicode = 9855 + computerRoll;
+            playerDice.innerHTML = "&#" + playerUnicode + ";";
+            computerDice.innerHTML = "&#" + computerUnicode + ";";
+
+            if(--rebound === 0) {
+                clearInterval(timerId);
+
+                (playerRoll > computerRoll) ? playerScore++ : computerScore++;
+
+                playerTitle.innerText = `Player score : ${playerScore}`;
+                computerTitle.innerText = `Computer score : ${computerScore}`;
+
+                rollButton.disabled = false;
+            }
+        }
+
 });
 
 function randomDice(){
-    return Math.floor(Math.random() * 6 + 1);
+    return random(1,6);
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + 1);
 }
