4a3fda602fdca79374f0d68a4ebf60ac366ec2ca
Marco Guilmette <marco.guilmette@softbiomed.com>
2019-05-13 20:43:58 -0400

    Rock Paper Scissors

--- Part09-Rock Paper Scissor/app.js
+++ Part09-Rock Paper Scissor/app.js
@@ -1,26 +1,44 @@
-const objects = ["Rock", "Paper", "Scissor"];
+var score = document.getElementById("score");
+var selection = document.getElementById("selection");
+var winner = document.getElementById("winner");
 
-function createMatrix() {
-    let array = new Array(3);
-    array.forEach( (a)=> {
-        a = [1,1,1];
-    } );
-}
+const objects = ["Rock", "Paper", "Scissor"];
 
 var matrix = [
   [0 , -1, 1],
   [1, 0, -1],
-  [0]
+  [-1, 1, 0]
 ];
+
+var computerScore = 0;
+var playerScore = 0;
 
 document.querySelectorAll( "button" ).forEach( (e, i)=> {
 
     e.index = i;
-    e.addEventListener("click", () => {
+    e.addEventListener("click", (event) => {
 
         let computerIndex = Math.floor(Math.random() * objects.length);
+        let playerIndex = e.index;
 
+        selection.innerText = `Player[${objects[playerIndex]}] vs Computer[${objects[computerIndex]}]`;
 
+        let result = matrix[playerIndex][computerIndex];
+        switch(result){
+            case -1:
+                winner.innerText =  "You lose !";
+                computerScore++;
+                break;
+            case 0:
+                winner.innerText = "Tie, try again !";
+                break;
+            case 1:
+                winner.innerText = "You win !";
+                playerScore++;
+                break;
+        }
+
+        score.innerText = `Player[${playerScore}] & Computer[${computerScore}]`;
 
     });
 
