b6a43ad847b4c0a95447fdefcfdb185432fa77b8
Marco Guilmette <hk78@live.ca>
2019-05-16 12:37:46 -0400

    Bricks game added

--- Part55-Bricks/script.js
+++ Part55-Bricks/script.js
@@ -1,5 +1,6 @@
 
 const BRICK_WIDTH = 100;
+const TOP_SPACE = 30;
 
 let board = document.getElementById("board");
 let rect = board.getBoundingClientRect();
@@ -8,8 +9,17 @@ function buildBricks(row) {
     let count = Math.floor( rect.width / BRICK_WIDTH )
     let x = (rect.width % BRICK_WIDTH) / 2
 
-    for(  ) {
-
+    for(let j =0; j < row; j++){
+        for (let i = 0; i < count; i++) {
+            let brick = document.createElement("div");
+            brick.classList.add( "brick" );
+            brick.style.left = (i * BRICK_WIDTH) + x + "px" ;
+            brick.style.top = (j * 50) + TOP_SPACE + "px";
+            board.appendChild(brick);
+        }
     }
 
+
 }
+
+buildBricks(5);
